e865e2cc1996a3862e2505c775b57a16d571a6cb
hamad <nhamad919@gmail.com>
2024-05-05 23:44:41 +0400

    Added the header and footer with the logo for the website

---
+++ src/components/Layout/Footer/Footer.jsx
@@ -0,0 +1,197 @@
+import { NavLink } from "react-router-dom";
+import logoImg from "../../images/Logo.png";
+import rectangle from "../../images/rectangle.svg";
+import rectangleTwo from "../../images/rectangleTwo.svg";
+import location from "../../images/location.png";
+import { Phone, Mail, Clock9, MapPin } from "lucide-react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="bg-navyBlue">
+        <div className="mx-auto max-w-[120rem] space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
+          <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+            <div>
+              <div className="w-56 h-56">
+                <img src={logoImg} alt="logo" />
+              </div>
+
+              <p className="mt-4 max-w-xs text-white font-lexendDeca">
+                &quot;Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Esse non cupiditate quae nam molestias.&quot;
+              </p>
+
+              <h2 className="text-white text-xl mt-4 font-daysOne">
+                Follow Us
+              </h2>
+              <img src={rectangle} alt="rectangle" className="mt-5" />
+              <ul className="mt-[25px] flex gap-6">
+                <li>
+                  <a
+                    href="#"
+                    rel="noreferrer"
+                    target="_blank"
+                    className="text-white transition hover:opacity-75"
+                  >
+                    <span className="sr-only">Facebook</span>
+
+                    <svg
+                      className="h-11 w-11"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="grid grid-cols-1 gap-9 sm:grid-cols-2 lg:col-span-2 lg:grid-cols-3">
+              <div>
+                <p className="font-[1.25rem] text-white font-daysOne uppercase">
+                  Links
+                </p>
+                <img
+                  src={rectangle}
+                  alt="rectangle"
+                  className="w-[100px] mt-5"
+                />
+
+                <ul className="mt-[50px] text-xl">
+                  <li className="mb-4">
+                    <NavLink
+                      to=""
+                      className="text-white font-lexendDeca text-[1.125rem] transition hover:opacity-75"
+                    >
+                      {" "}
+                      Home{" "}
+                    </NavLink>
+                  </li>
+
+                  <li className="mb-4">
+                    <a
+                      href="/about"
+                      className="text-white font-lexendDeca text-[1.125rem] transition hover:opacity-75"
+                    >
+                      {" "}
+                      About{" "}
+                    </a>
+                  </li>
+
+                  <li className="mb-4">
+                    <a
+                      href="/services"
+                      className="text-white font-lexendDeca text-[1.125rem] transition hover:opacity-75"
+                    >
+                      {" "}
+                      Services{" "}
+                    </a>
+                  </li>
+
+                  <li className="mb-4">
+                    <a
+                      href="/gallery"
+                      className="text-white font-lexendDeca text-[1.125rem] transition hover:opacity-75"
+                    >
+                      {" "}
+                      Gallery{" "}
+                    </a>
+                  </li>
+
+                  <li className="mb-4">
+                    <a
+                      href="/bookings"
+                      className="text-white font-lexendDeca text-[1.125rem] transition hover:opacity-75"
+                    >
+                      {" "}
+                      Bookings{" "}
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/forum"
+                      className="text-white font-lexendDeca text-[1.125rem] transition hover:opacity-75"
+                    >
+                      {" "}
+                      Forum{" "}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div>
+                <p className="font-[1.25rem] text-white font-daysOne uppercase">
+                  Contact Info
+                </p>
+
+                <img src={rectangleTwo} alt="rectangleTwo" className="mt-5" />
+
+                <ul className="mt-[50px] text-sm">
+                  <li className="mb-9">
+                    <a
+                      href="#"
+                      className="flex text-white font-lexendDeca text-[1.125rem] transition hover:opacity-75"
+                    >
+                      <Phone className="mr-3" /> 052 304 1850{" "}
+                    </a>
+                  </li>
+
+                  <li className="mb-9">
+                    <a
+                      href="#"
+                      className="flex text-white font-lexendDeca text-[1.1rem] transition hover:opacity-75"
+                    >
+                      {" "}
+                      <Mail className="mr-3" /> mnlabrosauto@gmail.com{" "}
+                    </a>
+                  </li>
+
+                  <li className="mb-9">
+                    <a
+                      href="#"
+                      className="flex text-white font-lexendDeca text-[1.125rem] transition hover:opacity-75"
+                    >
+                      <Clock9 className="mr-3" /> Monday-Sunday: 8-AM - 8PM{" "}
+                      <br /> Friday: Closed{" "}
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      href="#"
+                      className="flex text-white font-lexendDeca text-[1.125rem] transition hover:opacity-75"
+                    >
+                      <MapPin className="mr-3" /> Warehouse No. 2 21st Street -
+                      Al <br /> Qusais Industrial Area - Al
+                      <br />
+                      Qusais Industrial Area 1 - Dubai{" "}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <p className="font-[1.25rem] text-white font-daysOne uppercase">
+                  Location
+                </p>
+                <img src={rectangle} alt="rectangle" className="mt-5" />
+                <img src={location} alt="location" className="mt-12" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <p className="text-[1rem] font-daysOne text-white text-center bg-darkRed w-full h-[76px] flex justify-center items-center">
+          &copy; 2024. Manila Brothers Autoworks Filipino Garage. All rights
+          reserved.
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/components/Layout/Header/Header.jsx
@@ -0,0 +1,221 @@
+import { useState } from "react";
+import { Menu, X, ChevronDown } from "lucide-react";
+import { NavLink } from "react-router-dom";
+import logoImg from "../../images/Logo.png";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isAboutOpen, setIsAboutOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const toggleAbout = () => {
+    setIsAboutOpen(!isAboutOpen);
+  };
+  return (
+    <div className="relative w-full bg-darkBlue shadow-darkBlue shadow-lg">
+      <div className="mx-auto flex max-w-[120rem] items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
+        <div className="inline-flex items-center space-x-2">
+          <span className="w-[90px] h-[90px] relative top-4 after:shadow-darkBlue after:shadow-lg after:bg-darkBlue after:absolute after:p-[54px] after:-right-2.5 after:-top-0.5 after:rounded-full z-20 after:-z-10">
+            <img src={logoImg} alt="logo" />
+          </span>
+          <span className="font-stalinist text-white text-[16px] first-line:text-[24px] pl-2 leading-1 items-end">
+            Manila <br /> Brothers
+          </span>
+        </div>
+        <div className="hidden grow items-start lg:flex justify-end">
+          <ul className="mx-12 inline-flex space-x-12">
+            <li>
+              <NavLink
+                to=""
+                className={({ isActive }) =>
+                  `inline-flex items-center px-3 py-2 rounded-md text-sm text-white font-lexendDeca uppercase ${isActive ? "bg-mainRed shadow-mainRed shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_10px_#08f,0_0_20px_#08f]" : ""}`
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <span
+                className="inline-flex items-center px-3 py-2 text-sm text-white font-lexendDeca uppercase cursor-pointer"
+                onClick={toggleAbout}
+              >
+                About
+                <span>
+                  <ChevronDown className="ml-2 h-4 w-4" />
+                </span>
+              </span>
+              {isAboutOpen && (
+                <ul className="absolute rounded-lg mt-2 py-1 shadow-lg z-10 bg-lightBlue">
+                  <li>
+                    <NavLink
+                      to="/about"
+                      className={({ isActive }) =>
+                        `block px-4 py-2 text-white font-lexendDeca uppercase ${isActive ? "bg-mainRed" : ""}`
+                      }
+                    >
+                      About
+                    </NavLink>
+                    <NavLink
+                      to="/services"
+                      className={({ isActive }) =>
+                        `block px-4 py-2 text-white font-lexendDeca uppercase ${isActive ? "bg-mainRed" : ""}`
+                      }
+                    >
+                      Services
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/contact"
+                      className={({ isActive }) =>
+                        `block px-4 py-2 text-white font-lexendDeca uppercase ${isActive ? "bg-mainRed" : ""}`
+                      }
+                    >
+                      Gallery
+                    </NavLink>
+                  </li>
+                </ul>
+              )}
+            </li>
+            <li>
+              <NavLink
+                to="/forum"
+                className={({ isActive }) =>
+                  `inline-flex items-center px-3 py-2 rounded-md text-sm text-white font-lexendDeca uppercase ${isActive ? "bg-mainRed shadow-mainRed shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_10px_#08f,0_0_20px_#08f]" : ""}`
+                }
+              >
+                Forum
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/bookings"
+                className={({ isActive }) =>
+                  `inline-flex items-center px-3 py-2 rounded-md text-sm text-white font-lexendDeca uppercase ${isActive ? "bg-mainRed shadow-mainRed shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_10px_#08f,0_0_20px_#08f]" : ""}`
+                }
+              >
+                Bookings
+              </NavLink>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="rounded-md px-3 py-2 text-sm font-lexendDeca uppercase text-black bg-primaryBg shadow-primaryBg shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_10px_#08f,0_0_20px_#08f]"
+              >
+                <NavLink to="/contact">Contact Us</NavLink>
+              </button>
+            </li>
+            <li>
+              <NavLink
+                to="login"
+                className={({ isActive }) =>
+                  `inline-flex items-center px-3 py-2 rounded-md text-sm text-white font-lexendDeca uppercase ${isActive ? "bg-mainRed shadow-mainRed shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_10px_#08f,0_0_20px_#08f]" : ""}`
+                }
+              >
+                Log In
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="lg:hidden">
+          <Menu
+            onClick={toggleMenu}
+            className="h-6 w-6 cursor-pointer text-white"
+          />
+        </div>
+        {isMenuOpen && (
+          <div className="absolute inset-x-0 top-0 z-50 origin-top-right transform p-2 transition lg:hidden">
+            <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+              <div className="px-5 pb-6 pt-5 bg-lightBlue">
+                <div className="flex items-center justify-between">
+                  <div className="inline-flex items-center space-x-2">
+                    <span className="w-[90px] h-[90px]">
+                      <img src={logoImg} alt="logo" />
+                    </span>
+                    <span className="font-stalinist text-white">
+                      Manilla <br /> Brothers
+                    </span>
+                  </div>
+                  <div className="-mr-2">
+                    <button
+                      type="button"
+                      onClick={toggleMenu}
+                      className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                    >
+                      <span className="sr-only">Close menu</span>
+                      <X className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                  </div>
+                </div>
+                <div className="mt-6">
+                  <nav className="grid gap-y-4">
+                    <NavLink
+                      to="/"
+                      className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-mainRed"
+                    >
+                      <span className="ml-3 text-base font-medium text-white font-lexendDeca uppercase">
+                        Home
+                      </span>
+                    </NavLink>
+                    <NavLink
+                      to="/about"
+                      className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-mainRed"
+                    >
+                      <span className="ml-3 text-base font-medium text-white font-lexendDeca uppercase">
+                        About
+                      </span>
+                    </NavLink>
+                    <NavLink
+                      to="/services"
+                      className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-mainRed"
+                    >
+                      <span className="ml-3 text-base font-medium text-white font-lexendDeca uppercase">
+                        Services
+                      </span>
+                    </NavLink>
+                    <NavLink
+                      to="/contact"
+                      className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-mainRed"
+                    >
+                      <span className="ml-3 text-base font-medium text-white font-lexendDeca uppercase">
+                        Gallery
+                      </span>
+                    </NavLink>
+                    <NavLink
+                      to="/forum"
+                      className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-mainRed"
+                    >
+                      <span className="ml-3 text-base font-medium text-white font-lexendDeca uppercase">
+                        Forum
+                      </span>
+                    </NavLink>
+                    <NavLink
+                      to="/bookings"
+                      className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-mainRed"
+                    >
+                      <span className="ml-3 text-base font-medium text-white font-lexendDeca uppercase">
+                        Bookings
+                      </span>
+                    </NavLink>
+                  </nav>
+                </div>
+                <div className="mt-2 space-y-2">
+                  <button
+                    type="button"
+                    className="rounded-md px-3 py-2 text-sm font-semibold text-white hover:bg-mainRed font-lexendDeca uppercase"
+                  >
+                    Log In
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
