c28f56a1e790833c962cabfd4e1da6b36b5f871d
hamad <nhamad919@gmail.com>
2024-05-29 17:42:55 +0400

    Added different section to the homepage

--- src/components/pages/Gallery/Gallery.jsx
+++ src/components/pages/Gallery/Gallery.jsx
@@ -1,10 +1,9 @@
 function Gallery() {
-    return(
-        <>
-            <span className="text-3xl">GALLERY</span>
-        </>
-    )
-
+  return (
+    <>
+      <span className="text-3xl">GALLERY</span>
+    </>
+  );
 }
 
 export default Gallery;
