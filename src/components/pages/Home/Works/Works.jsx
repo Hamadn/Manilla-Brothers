39b385548b85bd2075b07d51e312c23a8c3e29ef
Hamadn <nhamad919@gmail.com>
2024-11-06 09:46:04 +0400

    Fixed a bug
    fixed the text going out of the box in the featured works on homepage
    and made the button align left on small screens

--- src/components/pages/Home/Works/Works.jsx
+++ src/components/pages/Home/Works/Works.jsx
@@ -5,8 +5,10 @@ function Works() {
   return (
     <section className="relative bg-featured-works-pattern bg-center w-full h-full p-16 bg-cover flex flex-col items-center">
       <div className="absolute inset-0 bg-rgbaLightRed z-10"></div>
-      <div className="text-2xl lg:text-5xl text-white drop-shadow-neonWhite w-full h-96 md:h-36  max-w-[120rem] font-daysOne text-left mt-1 lg:mt-2 z-20 relative">
-        <h1 className="text-[20px] md:text-2xl lg:text-4xl uppercase">Check out our featured works!</h1>
+      <div className="text-2xl lg:text-5xl text-white drop-shadow-neonWhite w-full h-max max-w-[120rem] font-daysOne text-left mt-1 lg:mt-2 z-20 relative">
+        <h1 className="text-[20px] md:text-2xl lg:text-4xl uppercase">
+          Check out our featured works!
+        </h1>
         <p className="text-white text-base drop-shadow-none font-lexendDeca z-20 pt-3">
           Explore how Manila Brothers Autoworks brings expert craftsmanship and
           meticulous care to every project.
@@ -15,7 +17,7 @@ function Works() {
           attention to detail.
         </p>
       </div>
-      <Button className="bg-white uppercase shadow-glowWhite font-lexendDeca text-[14px] sm:text-xl text-black mt-4 z-20 h-[50px] sm:h-[60px] w-[180px] sm:w-[230px] self-end">
+      <Button className="bg-white uppercase shadow-glowWhite font-lexendDeca text-[14px] sm:text-xl text-black mt-4 z-20 self-start justify-start h-[50px] sm:h-[60px] w-[180px] sm:w-[230px] md:self-end">
         <Link to="/gallery">Visit Gallery</Link>
       </Button>
     </section>
